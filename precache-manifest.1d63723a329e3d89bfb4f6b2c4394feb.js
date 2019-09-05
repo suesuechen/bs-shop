cf8caf42525263bda4c88421ccf39fe9ab5fc662
SUE <chiachisusu@gmail.com>
2019-09-05 12:31:14 +0800

    deploy

---
+++ precache-manifest.1d63723a329e3d89bfb4f6b2c4394feb.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "18b2728ea67a48384aee",
+    "url": "/bs-shop/js/chunk-2d210a66.9b7b1881.js"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/bs-shop/robots.txt"
+  },
+  {
+    "revision": "3af1bd8e554008d8b865",
+    "url": "/bs-shop/css/chunk-4b844840.c3000840.css"
+  },
+  {
+    "revision": "8356c529fd2eba55b534",
+    "url": "/bs-shop/css/chunk-0780384d.f8900127.css"
+  },
+  {
+    "revision": "19d9da8687b0be7568c2",
+    "url": "/bs-shop/css/chunk-2c00df2e.87b515ef.css"
+  },
+  {
+    "revision": "19d9da8687b0be7568c2",
+    "url": "/bs-shop/js/chunk-2c00df2e.24f5118b.js"
+  },
+  {
+    "revision": "57fc6b9535d6559c9c0f",
+    "url": "/bs-shop/js/chunk-2d0a4038.b27f8603.js"
+  },
+  {
+    "revision": "241c9d5f299bd813638d",
+    "url": "/bs-shop/js/chunk-2d0bd1a5.593ad409.js"
+  },
+  {
+    "revision": "733ea934b876d259548d",
+    "url": "/bs-shop/js/chunk-2d0ce771.0dd5e45c.js"
+  },
+  {
+    "revision": "535c98f70dd44a8265db",
+    "url": "/bs-shop/js/chunk-2d20efe4.1bbcaac4.js"
+  },
+  {
+    "revision": "7b8fd800479e60bb52ac",
+    "url": "/bs-shop/js/app.d4377b84.js"
+  },
+  {
+    "revision": "8356c529fd2eba55b534",
+    "url": "/bs-shop/js/chunk-0780384d.9cc1f5a2.js"
+  },
+  {
+    "revision": "3af1bd8e554008d8b865",
+    "url": "/bs-shop/js/chunk-4b844840.d90a35da.js"
+  },
+  {
+    "revision": "ba462ffdaa893211d440",
+    "url": "/bs-shop/css/chunk-4fcff689.bf525715.css"
+  },
+  {
+    "revision": "ba462ffdaa893211d440",
+    "url": "/bs-shop/js/chunk-4fcff689.e4d9f75d.js"
+  },
+  {
+    "revision": "5c848fa85052dcf37b1e",
+    "url": "/bs-shop/css/chunk-8b35c446.263811af.css"
+  },
+  {
+    "revision": "5c848fa85052dcf37b1e",
+    "url": "/bs-shop/js/chunk-8b35c446.19f5343b.js"
+  },
+  {
+    "revision": "0921061d95c848845ba2",
+    "url": "/bs-shop/css/chunk-vendors.472c2b9a.css"
+  },
+  {
+    "revision": "0921061d95c848845ba2",
+    "url": "/bs-shop/js/chunk-vendors.290bf94e.js"
+  },
+  {
+    "revision": "84263cf1d7907a5e0eddf38168184fb5",
+    "url": "/bs-shop/index.html"
+  },
+  {
+    "revision": "7b8fd800479e60bb52ac",
+    "url": "/bs-shop/css/app.6a6f5f56.css"
+  }
+];
